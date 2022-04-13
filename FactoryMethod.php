c321c696e6c57747aa59ca05a3798de2aeb113b9
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2022-04-13 11:11:26 +0430

    add setters

--- FactoryMethod.php
+++ FactoryMethod.php
@@ -2,8 +2,10 @@
 // User interface
 interface UserDetailInterface
 {
-    public function getName(): string;
-    public function getEmail(): string;
+    public function setName($name);
+    public function setEmail($email);
+    public function getName();
+    public function getEmail();
 }
 
 // User classes - with Admin - Candidate - Employer Roles
@@ -12,18 +14,22 @@ class Admin implements UserDetailInterface
     private $name;
     private $email;
 
-    public function __construct($name, $email)
+    public function setName($name)
     {
         $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function getName(): string
+    public function getName()
     {
         return 'Admin with name: ' . $this->name;
     }
 
-    public function getEmail(): string
+    public function getEmail()
     {
         return 'Admin with email: ' . $this->email;
     }
@@ -34,18 +40,22 @@ class Candidate implements UserDetailInterface
     private $name;
     private $email;
 
-    public function __construct($name, $email)
+    public function setName($name)
     {
         $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function getName(): string
+    public function getName()
     {
         return 'Candidate with name: ' . $this->name;
     }
 
-    public function getEmail(): string
+    public function getEmail()
     {
         return 'Candidate with email: ' . $this->email;
     }
@@ -56,18 +66,22 @@ class Employer implements UserDetailInterface
     private $name;
     private $email;
 
-    public function __construct($name, $email)
+    public function setName($name)
     {
         $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function getName(): string
+    public function getName()
     {
         return 'Employer with name: ' . $this->name;
     }
 
-    public function getEmail(): string
+    public function getEmail()
     {
         return 'Employer with email: ' . $this->email;
     }
@@ -76,23 +90,23 @@ class Employer implements UserDetailInterface
 // Interface for factory class
 interface UserCreateInterface
 {
-    public static function create($role, $name, $email);
+    public static function create($role);
 }
 
 // Factory class for create user
 class UserCreateFactory implements UserCreateInterface
 {
-    public static function create($role, $name, $email)
+    public static function create($role)
     {
         switch ($role) {
             case 'Admin':
-                return new Admin($name, $email);
+                return new Admin();
                 break;
             case 'Candidate':
-                return new Candidate($name, $email);
+                return new Candidate();
                 break;
             case 'Employer':
-                return new Employer($name, $email);
+                return new Employer();
                 break;
             default:
                 throw new Exception('This user role is not valid');
@@ -103,9 +117,31 @@ class UserCreateFactory implements UserCreateInterface
 // Client code:
 $users = [];
 
-array_push($users, UserCreateFactory::create('Admin', 'sara', 'sara@gmail.com'));
-array_push($users, UserCreateFactory::create('Candidate', 'behroz', 'behroz@gmail.com'));
-array_push($users, UserCreateFactory::create('Employer', 'hamid', 'hamid@gmail.com'));
+$usersList = [
+    [
+        'name' => 'sara',
+        'email' => 'sara@gmail.com',
+        'role' => 'Admin'
+    ],
+    [
+        'name' => 'behroz',
+        'email' => 'behroz@gmail.com',
+        'role' => 'Candidate'
+    ],
+    [
+        'name' => 'hamid',
+        'email' => 'hamid@gmail.com',
+        'role' => 'Employer'
+    ],
+];
+
+foreach($usersList as $userListItem) {
+    $user = UserCreateFactory::create($userListItem['role']);
+    $user->setName($userListItem['name']);
+    $user->setEmail($userListItem['email']);
+
+    array_push($users, $user);
+}
 
 foreach($users as $user) {
     echo $user->getName();
